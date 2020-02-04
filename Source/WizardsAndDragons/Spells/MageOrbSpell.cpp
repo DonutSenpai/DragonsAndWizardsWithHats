60a1e57353a5dbe9e1365b957dc7d70d5476ded2
Antonia Kiili <33453826+DonutSenpai@users.noreply.github.com>
2020-02-04 12:18:56 +0100

    The Mage Orb now always goes to the max range point
    It calculates it from the direction of where you are clicking to activate the spell relative to your character

--- Source/WizardsAndDragons/Spells/MageOrbSpell.cpp
+++ Source/WizardsAndDragons/Spells/MageOrbSpell.cpp
@@ -2,9 +2,15 @@
 
 void UMageOrbSpell::InternalCastSpell(FVector TargetLocation)
 {
-	OnCastSpellEffectInternal(TravelTime, TargetLocation);
-	GetWorld()->GetTimerManager().SetTimer(HitExplosionHandle, FTimerDelegate::CreateUObject(this, &UMageOrbSpell::HitExplosion, TargetLocation), TravelTime, false);
+	FVector AdjustedTargetLocation = TargetLocation - GetOwner()->GetActorLocation();	
+	AdjustedTargetLocation.Z = 0.0f;	
+	AdjustedTargetLocation.Normalize();	
+	AdjustedTargetLocation *= Range;	
+	AdjustedTargetLocation += GetOwner()->GetActorLocation();
 
+
+	OnCastSpellEffectInternal(TravelTime, AdjustedTargetLocation);
+	GetWorld()->GetTimerManager().SetTimer(HitExplosionHandle, FTimerDelegate::CreateUObject(this, &UMageOrbSpell::HitExplosion, TargetLocation), TravelTime, false);
 }
 
 void UMageOrbSpell::InternalDealDamage(const TArray<AActor*>& SpellTargets)
